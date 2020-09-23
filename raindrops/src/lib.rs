fab3525e205b9167239b1e386b53defc995907a8
Dominik Rys <dominik.j.rys@gmail.com>
2020-09-23 16:22:34 +0100

    Improve raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,17 +1,11 @@
 pub fn raindrops(n: u32) -> String {
-    let mut rain_str = String::new();
+    let raindrop_factor_strings = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
-    if n % 3 == 0 {
-        rain_str.push_str("Pling")
-    }
-
-    if n % 5 == 0 {
-        rain_str.push_str("Plang")
-    }
-
-    if n % 7 == 0 {
-        rain_str.push_str("Plong")
-    }
+    let rain_str: String = raindrop_factor_strings
+        .iter()
+        .filter(|&(num, _)| n % num == 0)
+        .map(|&(_, s)| s)
+        .collect();
 
     return if rain_str.is_empty() {
         n.to_string()

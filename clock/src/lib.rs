91a25ff1ed715d242fd879be3471574a7d783d7c
Dominik Rys <dominik.j.rys@gmail.com>
2020-09-26 16:23:37 +0100

    Improve clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -11,10 +11,8 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i64, minutes: i64) -> Self {
         Clock {
-            minutes: (((hours * MINUTES_IN_HOUR + minutes) /* Convert hours to minutes and get total minutes */
-            % MINUTES_IN_DAY) /* Account for if total minutes have overflown over a day */
-            + MINUTES_IN_DAY) /* Add minutes in a day in case total minutes are negative */
-            % MINUTES_IN_DAY, /* Again, account for if total minutes have overflown over a day */
+            // Use rem_euclid() to calculate non-negative remainder
+            minutes: (hours * MINUTES_IN_HOUR + minutes).rem_euclid(MINUTES_IN_DAY),
         }
     }
 

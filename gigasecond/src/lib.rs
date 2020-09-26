f7ef5b86bb6979c2fd75f4286f80123d0b00b4f4
Dominik Rys <dominik.j.rys@gmail.com>
2020-09-26 09:08:53 +0100

    Update gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,8 +1,8 @@
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, Utc};
 
 const GIGASECOND: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.checked_add_signed(Duration::seconds(GIGASECOND)).expect("Overflow!")
+    start + Duration::seconds(GIGASECOND)
 }

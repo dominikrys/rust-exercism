42da607c1a1c4bc9b5ce195e53a7cb6ecc0e7c85
Dominik Rys <dominik.j.rys@gmail.com>
2020-09-27 15:34:17 +0100

    Remove redundant comment

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,4 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    // rev() returns an iterator, so it needs to be transformed into a collection using .collect()
     input.chars().rev().collect::<String>()
 }
